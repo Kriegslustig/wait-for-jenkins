c093e57a0afa04ed341ea62defe6375c8fa961f9
Luca Nils Schmid <me@lucaschmid.net>
2017-11-28 22:27:00 +0100

    feat(index): implement CLI

---
+++ index.js
@@ -0,0 +1,15 @@
+const pollJenkinsFeed = require('./lib')
+
+const JENKINS_BASE_URL = process.env.JENKINS_BASE_URL
+const [,, projectName] = process.argv
+
+
+pollJenkinsFeed(JENKINS_BASE_URL, projectName, (error, newTitle) => {
+  if (error) {
+    console.error(error)
+    process.exit(1)
+  }
+
+  console.log(newTitle)
+  process.exit(0)
+})
